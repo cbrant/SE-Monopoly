9b0abc6852f8bed9e356702d3c6c6426b8bcb750
cbrant <cbrant@nd.edu>
2015-10-01 17:52:40 -0400

    Started work on the game panel, adding dice button

--- Monopoly/src/main/GamePanel.java
+++ Monopoly/src/main/GamePanel.java
@@ -1,17 +1,19 @@
 package main;
 
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Image;
+import java.awt.Insets;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JButton;
-import java.awt.GridLayout;
+import javax.swing.JPanel;
 import javax.swing.JSeparator;
 
 public class GamePanel extends JPanel {
-
-	public JButton rollButton;
 
 	private MainWindow parent;
 
@@ -20,22 +22,42 @@ public class GamePanel extends JPanel {
 	 */
 
 	public GamePanel(MainWindow par) {
+		setBackground(new Color(255, 250, 205));
 		this.parent = par;
-		
-		setLayout(null);
 
 		setBounds(100, 100, 650, 725);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		
+		JButton btnNewButton = new JButton("");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.fill = GridBagConstraints.BOTH;
+		gbc_btnNewButton.gridheight = 2;
+		gbc_btnNewButton.gridwidth = 2;
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton.gridx = 10;
+		gbc_btnNewButton.gridy = 8;
+		Image img = new ImageIcon(this.getClass().getResource("/dice.png")).getImage();
+		btnNewButton.setIcon(new ImageIcon(img));
+		add(btnNewButton, gbc_btnNewButton);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBackground(new Color(0, 0, 0));
+		separator.setForeground(new Color(0, 0, 0));
+		GridBagConstraints gbc_separator = new GridBagConstraints();
+		gbc_separator.fill = GridBagConstraints.BOTH;
+		gbc_separator.insets = new Insets(0, 0, 5, 5);
+		gbc_separator.gridx = 0;
+		gbc_separator.gridy = 18;
+		gbc_separator.gridwidth = 21;
+		add(separator, gbc_separator);
 
 
-		rollButton = new JButton("Roll");
-		rollButton.setBounds(254, 221, 158, 108);
-		add(rollButton);
-
-		JLabel background = new JLabel("");
-		Image img = new ImageIcon(this.getClass().getResource("/GameScreenBackground.png")).getImage();
-		background.setIcon(new ImageIcon(img));
-		background.setBounds(0, 0, 650, 725);
-		add(background);
 
 	}
 }
