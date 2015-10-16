964deebb40ea0dfda32b3e60386d9ee581f78f6c
Marcellus Diederich <mdiederi@nd.edu>
2015-10-16 11:30:24 -0400

    Pushing MoveSpecialCard

---
+++ Monopoly/src/main/MoveSpecialCard.java
@@ -0,0 +1,27 @@
+package main;
+
+public class MoveSpecialCard extends SpecialCard{
+
+	private int amount;
+	public MoveSpecialCard(String type, String line, int spacenum) {
+		super(type, line);
+		amount = spacenum;
+	}
+	
+	public void act(Player p, int playersOut, GamePanel eng) {
+		if(amount < 0) {	//only happens on the move back 3 spaces card, no chance of going backwards passed go
+			eng.movePlayer(amount);		//amount should always be -3
+		} 
+		else {		//instead of a distance, indicates a space (0 is go)
+			int location = p.getCurrLocation();
+			if(location >= amount){
+				int toGo = eng.getParentFrame().spaces.size()-location;
+				int toSpace = amount;
+				eng.movePlayer(toGo+toSpace);
+			} 
+			else {
+				eng.movePlayer(amount-location);
+			}
+		}
+	}
+}
