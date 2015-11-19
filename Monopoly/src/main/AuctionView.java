d6e4f79a5ec2d60fc0a058b81b4b28d4c0a15e11
Sam Rack <srack@nd.edu>
2015-11-18 19:58:41 -0500

    added auctionview files

---
+++ Monopoly/src/main/AuctionView.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class AuctionView extends JFrame {
+
+	public AuctionView() {
+		
+		
+	}
+	
+	
+	
+	
+}
---
+++ Monopoly/src/main/AuctionViewTest.java
@@ -0,0 +1,77 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AuctionViewTest {
+	public static AuctionView av;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		av = new AuctionView();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAuctionWindowVisible() {
+		// test AuctionView()
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testFirstPlayerBid() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	@Test
+	public void testRaisingBid() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	@Test
+	public void testPassingOnBid() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	@Test
+	public void testIllegalBid() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	@Test
+	public void testEndOfAuction() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	@Test
+	public void testPlayerRecievesProperty() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	@Test
+	public void testMoneyIsDeducted() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	@Test
+	public void testNoBid() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+}
