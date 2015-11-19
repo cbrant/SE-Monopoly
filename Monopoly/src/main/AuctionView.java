81035f19afadfb10b8b9e1805f2bea6b8edb7d48
Sam Rack <srack@nd.edu>
2015-11-18 20:38:25 -0500

    skeleton of auction view class

--- Monopoly/src/main/AuctionView.java
+++ Monopoly/src/main/AuctionView.java
@@ -2,41 +2,138 @@ package main;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class AuctionView extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					AuctionView frame = new AuctionView();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	private GamePanel parent;
+	
+	private int currBidder;
+	private int currBid;
+	private int highestBidder;
+	private int numPasses;
+	
 	/**
 	 * Create the frame.
 	 */
-	public AuctionView() {
+	public AuctionView(GamePanel par) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		parent = par;
+		// starting bidder is the next player
+		currBidder = (parent.currPlayer + 1) % 4;
+		currBid = 0;
+		highestBidder = -1;
+		numPasses = 0;
 	}
+	
+	public ActionListener bidMade = new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//numPasses = 0;
+			// get player who is currently bidding
+			
+			// parse the data in the entered field
+			// check this data for validity
+			//	(1) bid lower than currBid
+			//  (2) bid more than current player's bank
+			// 	(3) not an integer
+			// if either fails, give dialog saying error and return from function
+			//	without updating current bidder
+			int bid = 0;	// parse this from input box
+			if (!checkBidInput(bid)) return;
+			
+			// update currBid
+			updateBid(bid);
+			
+			// move to next player
+			updateBidder();
+			// graphics update here
+			updateBidGraphics();
+		}
+
+
+
+	};
+	
+	public boolean checkBidInput(int bid) {
+		// check that current player has enough money for this bid
+		// check that this bid is greater than current bid
+		// return false if either is not right
+		throw new NotImplementedException();
+	}
+
+
+	public void updateBidGraphics() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void updateBid(int bid) {
+
+	}
+
+
+	public void updateBidder() {
+		
+	}
+	
+	public ActionListener playerPasses = new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//++numPasses;
+			
+			// update to next bidder
+			updateBidder();
+			
+			// check if end of auction
+			if (isEndAuction()) {
+				// player is given property, money gets deducted, property owner gets set
+				bidWon();
+				// auction window closes
+				// next turn is called in GamePanel.optionToBuy() after window is created
+			}
+			else if (noBids()) {
+				// auction window closes
+				// next turn is called in GamePanel.optionToBuy()
+			}
+			else {
+				// graphics update
+				updateBidGraphics();				
+			}
+		}
+		
+	};
+
+	public boolean isEndAuction() {
+		// currBidder == highestBidder
+		return false;
+	}
+	
+	public boolean noBids() {
+		// numPasses == 4
+		return false;
+	}
+	
+	public void bidWon() {
+		// adds the property to the highestBidder's property list
+		// updates the property's owner
+		// deducts currBid from the highestBidder's bank
+	}
+	
 
 }
--- Monopoly/src/main/AuctionViewTest.java
+++ Monopoly/src/main/AuctionViewTest.java
@@ -9,6 +9,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class AuctionViewTest {
+	public static MainWindow main;
+	public static GamePanel game;
 	public static AuctionView av;
 	
 	@BeforeClass
@@ -21,7 +23,10 @@ public class AuctionViewTest {
 
 	@Before
 	public void setUp() throws Exception {
-		av = new AuctionView();
+		main = new MainWindow();
+		main.createAndShowGUI();
+		game = (GamePanel)main.cards.getComponent(2);
+		av = new AuctionView(game);
 	}
 
 	@After
@@ -36,41 +41,49 @@ public class AuctionViewTest {
 
 	@Test
 	public void testFirstPlayerBid() {
+		// something in bidMade -- maybe just call the helper function in there
 		fail("Not yet implemented"); // TODO
 	}
 	
 	@Test
 	public void testRaisingBid() {
+		// something in bidMade -- again maybe just a helper function
 		fail("Not yet implemented"); // TODO
 	}
 	
 	@Test
 	public void testPassingOnBid() {
+		// something in playerPasses -- maybe just helper
 		fail("Not yet implemented"); // TODO
 	}
 	
 	@Test
 	public void testIllegalBid() {
+		// in bidMade
 		fail("Not yet implemented"); // TODO
 	}
 	
 	@Test
 	public void testEndOfAuction() {
+		// in playerPasses
 		fail("Not yet implemented"); // TODO
 	}
 	
 	@Test
 	public void testPlayerRecievesProperty() {
+		// in playerPasses -- or helper that ends auction
 		fail("Not yet implemented"); // TODO
 	}
 	
 	@Test
 	public void testMoneyIsDeducted() {
+		// same as above
 		fail("Not yet implemented"); // TODO
 	}
 	
 	@Test
 	public void testNoBid() {
+		// in playerPasses 
 		fail("Not yet implemented"); // TODO
 	}
 	
